4953e5804aa78ddaf1b18162c77c8aaf4aa4cd67
Demian <89573873+djody28@users.noreply.github.com>
2024-12-04 20:32:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+let todos = [];
+
+const list = document.getElementById('todo-list');
+const itemCountSpan = document.getElementById('item-count');
+const uncheckedCountSpan = document.getElementById('unchecked-count');
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  const title = prompt('Введіть назву нової справи:');
+  if (title) {
+    const newTodo = {
+      id: Date.now(),
+      title,
+      checked: false
+    };
+    todos.push(newTodo);
+    render();
+    updateCounter();
+  }
+}
+
+function renderTodo(todo) {
+  return `
+    <li class="list-group-item">
+      <input type="checkbox" class="form-check-input me-2" id="${todo.id}" ${todo.checked ? 'checked' : ''} onChange="checkTodo(${todo.id})" />
+      <label for="${todo.id}">
+        <span class="${todo.checked ? 'text-success text-decoration-line-through' : ''}">${todo.title}</span>
+      </label>
+      <button class="btn btn-danger btn-sm float-end" onClick="deleteTodo(${todo.id})">delete</button>
+    </li>
+  `;
+}
+
+function render() {
+  list.innerHTML = todos.map(renderTodo).join('');
+}
+
+function updateCounter() {
+  itemCountSpan.textContent = todos.length;
+  uncheckedCountSpan.textContent = todos.filter(todo => !todo.checked).length;
+}
+
+function deleteTodo(id) {
+  todos = todos.filter(todo => todo.id !== id);
+  render();
+  updateCounter();
+}
+
+function checkTodo(id) {
+  const todo = todos.find(todo => todo.id === id);
+  if (todo) {
+    todo.checked = !todo.checked;
+    render();
+    updateCounter();
+  }
 }
